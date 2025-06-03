4b27d084ff89d994a52eac463ae7824c7b8edb36
Basma2423 <mayarbasma2423@gmail.com>
2025-06-03 07:12:15 +0300

    add example for injecting errors in inject_errors.py

---
+++ inject_errors.py
@@ -0,0 +1,17 @@
+from src.error_injection import inject_error, ErrorProbabilities
+
+text = "ثُمَّ ذَكَرَهَا هَكَذَا مِائَةً مِائَةً حَتَّى بَلَغَ أَلْفًا قَالَ ثُمَّ فِى كُلِّ مَا زَادَتْ مِائَةَ شَاةٍ شَاةٌ"
+print("Original text:", text)
+print("Text with errors:", inject_error(text, error_type="speech"))
+
+# try with custom probabilities
+
+ep = ErrorProbabilities(
+    phonetic=0.1,
+    diacritic=0.1,
+    no_error=0.9,
+    remove_diacritics=0.1
+)
+
+print("Original text:", text)
+print("Text with errors:", inject_error(text, error_type="speech", probs=ep))
